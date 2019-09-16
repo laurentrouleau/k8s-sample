5db99ea109dfa1dcb624210813ca163c248e2727
laurent rouleau <laurentrouleau87@gmail.com>
2019-09-16 15:00:01 +0200

    second commit

---
+++ docker/build.sh
@@ -0,0 +1,2 @@
+docker build -t laurentrouleau/helloworld-nodejs:0.1 .
+docker build -t laurentrouleau/helloworld-nodejs .
---
+++ docker/push.sh
@@ -0,0 +1,2 @@
+docker push laurentrouleau/helloworld-nodejs:0.1
+docker push laurentrouleau/helloworld-nodejs
